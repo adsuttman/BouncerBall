91cc8866177ca120da792d16f052a8b5aec5bbff
adsuttman <adsuttman@gmail.com>
2023-05-18 08:18:39 -0400

    rudimentary shoving

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -1,12 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Ball : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        GameObject pointer = GameObject.Find("Pointer");
+        Pointer script = pointer.GetComponent<Pointer>();
+        ArrayUtility.Add<GameObject>(ref script.balls, gameObject);
     }
 
     // Update is called once per frame
--- Assets/Pointer.cs
+++ Assets/Pointer.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Pointer : MonoBehaviour
 {
     GameObject obj;
+    public GameObject[] balls;
+    float shoveForce = 100;
+    float shoveDelay = 15;
+    float shoveTimer = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +19,26 @@ public class Pointer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //sets position of pointer to position of mouse
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = Camera.main.nearClipPlane;
         obj.transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+
+
+        if (Input.GetButtonDown("Fire1") && shoveTimer <= 0)
+        {
+            foreach (GameObject ball in balls)
+            {
+                Vector3 direction = ball.transform.position - obj.transform.position;
+                float distance = direction.magnitude;
+                if (distance < 10)
+                {
+                    ball.GetComponent<Rigidbody2D>().AddForce(direction.normalized * shoveForce, ForceMode2D.Impulse);
+                }
+            }
+            shoveTimer = shoveDelay;
+        }
+        shoveTimer -= 1;
     }
 
 }
