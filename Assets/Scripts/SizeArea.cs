4eaa8cb248f26de0b6f07c5d33b8fc5b39f23d70
adsuttman <adsuttman@gmail.com>
2023-05-31 09:01:43 -0400

    added grow/shrink areas

---
+++ Assets/Scripts/SizeArea.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeArea : MonoBehaviour
+{
+    public Vector3 minScale = new Vector3(0.15f,0.15f,0.15f);
+    public Vector3 maxScale = new Vector3(1f,1f,1f);
+    public Vector3 scaleAmount = new Vector3(0.2f,0.2f,0.2f);
+    public bool grow = true;
+    public LayerMask ballLayer;
+
+
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        GameObject obj = collider.gameObject;
+        Vector3 scale = obj.transform.localScale;
+        print(obj.name);
+        if (grow)
+        {
+            print("growing");
+            obj.transform.localScale = ClampScale(scale + scaleAmount * Time.deltaTime);
+        }
+        else
+        {
+            print("shrinking");
+            obj.transform.localScale = ClampScale(scale - scaleAmount * Time.deltaTime);
+        }
+    }
+    Vector3 ClampScale(Vector3 scale)
+    {
+       if (scale.x > maxScale.x)
+       {
+            return maxScale;
+       }
+       else if (scale.x < minScale.x)
+       {
+            return minScale;
+       }
+       else
+        {
+            return scale;
+        }
+    }
+}
